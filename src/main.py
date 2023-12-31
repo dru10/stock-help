3eb37a897a2a118277f1544f5629a7e50e112d38
horia.alex10 <horia.alex10@gmail.com>
2023-12-31 18:34:33 +0200

    evaluate on all epochs on all symbols at once

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 import torch
 import torch.nn as nn
@@ -14,7 +15,8 @@ from eval import (
 from train import train
 
 mode = "eval"
-symbols = ["^SPX", "^DAX", "^BET"]
+# symbols = ["^SPX", "^DAX", "^BET"]
+symbols = ["^SPX"]
 symbol = symbols[0]
 model_type = "LSTM1"
 batch_size = 32
@@ -22,113 +24,122 @@ epochs = 100
 lags = 10
 epoch = 90
 
+for symbol in symbols:
+    dataset = create_dataset(symbol)
 
-dataset = create_dataset(symbol)
+    lag_predictions = defaultdict(dict)
 
-lag_predictions = {}
+    for lags in [5, 10, 20]:
+        x_train, y_train = dataset["train"]["X"], dataset["train"]["Y"]
+        x_valid, y_valid = dataset["valid"]["X"], dataset["valid"]["Y"]
+        x_test, y_test = dataset["test"]["X"], dataset["test"]["Y"]
 
-for lags in [5, 10, 20]:
-    x_train, y_train = dataset["train"]["X"], dataset["train"]["Y"]
-    x_valid, y_valid = dataset["valid"]["X"], dataset["valid"]["Y"]
-    x_test, y_test = dataset["test"]["X"], dataset["test"]["Y"]
+        # Convert to tensors
+        x_train = torch.tensor(x_train[:, :lags], dtype=torch.float32)
+        y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
+        x_valid = torch.tensor(x_valid[:, :lags], dtype=torch.float32)
+        y_valid = torch.tensor(y_valid, dtype=torch.float32).unsqueeze(1)
+        x_test = torch.tensor(x_test[:, :lags], dtype=torch.float32)
+        y_test = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
 
-    # Convert to tensors
-    x_train = torch.tensor(x_train[:, :lags], dtype=torch.float32)
-    y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
-    x_valid = torch.tensor(x_valid[:, :lags], dtype=torch.float32)
-    y_valid = torch.tensor(y_valid, dtype=torch.float32).unsqueeze(1)
-    x_test = torch.tensor(x_test[:, :lags], dtype=torch.float32)
-    y_test = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
+        models_kwargs = {
+            "DNN1": {"input_shape": x_train.shape[1], "layers": [32, 32]},
+            "DNN2": {"input_shape": x_train.shape[1], "layers": [64, 64]},
+            "DNN3": {"input_shape": x_train.shape[1], "layers": [64, 32]},
+            "LSTM1": {"input_shape": x_train.shape[1]},
+            "LSTM2": {
+                "input_shape": x_train.shape[1],
+                "hidden": [50],
+                "layers": [25],
+                "num_layers": 2,
+            },
+            "LSTM3": {
+                "input_shape": x_train.shape[1],
+                "hidden": [64, 32],
+                "layers": [32, 16],
+                "num_layers": 4,
+                "dropout": 0.4,
+            },
+            "GRU1": {"input_shape": x_train.shape[1]},
+            "GRU2": {
+                "input_shape": x_train.shape[1],
+                "hidden": [50],
+                "layers": [25],
+                "num_layers": 2,
+            },
+            "GRU3": {
+                "input_shape": x_train.shape[1],
+                "hidden": [64, 32],
+                "layers": [32, 16],
+                "num_layers": 4,
+                "dropout": 0.4,
+            },
+        }
+        model = getattr(models, model_type[:-1])(**models_kwargs[model_type])
 
-    models_kwargs = {
-        "DNN1": {"input_shape": x_train.shape[1], "layers": [32, 32]},
-        "DNN2": {"input_shape": x_train.shape[1], "layers": [64, 64]},
-        "DNN3": {"input_shape": x_train.shape[1], "layers": [64, 32]},
-        "LSTM1": {"input_shape": x_train.shape[1]},
-        "LSTM2": {
-            "input_shape": x_train.shape[1],
-            "hidden": [50],
-            "layers": [25],
-            "num_layers": 2,
-        },
-        "LSTM3": {
-            "input_shape": x_train.shape[1],
-            "hidden": [64, 32],
-            "layers": [32, 16],
-            "num_layers": 4,
-            "dropout": 0.4,
-        },
-        "GRU1": {"input_shape": x_train.shape[1]},
-        "GRU2": {
-            "input_shape": x_train.shape[1],
-            "hidden": [50],
-            "layers": [25],
-            "num_layers": 2,
-        },
-        "GRU3": {
-            "input_shape": x_train.shape[1],
-            "hidden": [64, 32],
-            "layers": [32, 16],
-            "num_layers": 4,
-            "dropout": 0.4,
-        },
-    }
-    model = getattr(models, model_type[:-1])(**models_kwargs[model_type])
+        if mode == "train":
+            model.save_architecture(model_type)
 
-    if mode == "train":
-        model.save_architecture(model_type)
+        # Binary Cross Entropy Loss
+        criterion = nn.BCELoss()
 
-    # Binary Cross Entropy Loss
-    criterion = nn.BCELoss()
+        # Adam optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    # Adam optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    model_path = os.path.join(
-        "models",
-        model_type,
-        symbol,
-        "lags",
-        str(lags),
-        "batch_size",
-        str(batch_size),
-        "epochs",
-    )
-
-    if mode == "train":
-        train_loss, valid_loss = train(
-            model=model,
-            model_path=model_path,
-            train_valid=(x_train, y_train, x_valid, y_valid),
-            criterion=criterion,
-            optimizer=optimizer,
-            epochs=epochs,
-            batch_size=batch_size,
-        )
-    elif mode == "eval":
-        # for epoch in list(range(0, 100, 10)) + [epochs - 1]:
-        final_path = os.path.join(model_path, str(epoch))
-        model.load_state_dict(torch.load(os.path.join(final_path, "params.pt")))
-
-        predictions = predict_test_set(model, x_test, y_test)
-
-        lag_predictions[lags] = predictions
-
-        calculate_stats(predictions, y_test, final_path)
-
-        log_rets = dataset["test"]["log_ret"]
-        dates = dataset["test"]["dates"]
-
-        evaluate_predictions(
-            symbol=symbol,
-            model_path=final_path,
-            pred=predictions,
-            true=y_test,
-            log_close=log_rets,
-            dates=dates,
+        model_path = os.path.join(
+            "models",
+            model_type,
+            symbol,
+            "lags",
+            str(lags),
+            "batch_size",
+            str(batch_size),
+            "epochs",
         )
 
-if mode == "eval":
-    evaluate_all_lags(
-        dates, log_rets, lag_predictions, symbol, model_type, batch_size, epoch
-    )
+        if mode == "train":
+            train_loss, valid_loss = train(
+                model=model,
+                model_path=model_path,
+                train_valid=(x_train, y_train, x_valid, y_valid),
+                criterion=criterion,
+                optimizer=optimizer,
+                epochs=epochs,
+                batch_size=batch_size,
+            )
+        elif mode == "eval":
+            for epoch in list(range(0, epochs, 10)) + [epochs - 1]:
+                final_path = os.path.join(model_path, str(epoch))
+                model.load_state_dict(
+                    torch.load(os.path.join(final_path, "params.pt"))
+                )
+
+                predictions = predict_test_set(model, x_test, y_test)
+
+                lag_predictions[epoch][lags] = predictions
+
+                calculate_stats(predictions, y_test, final_path)
+
+                log_rets = dataset["test"]["log_ret"]
+                dates = dataset["test"]["dates"]
+
+                evaluate_predictions(
+                    symbol=symbol,
+                    model_path=final_path,
+                    pred=predictions,
+                    true=y_test,
+                    log_close=log_rets,
+                    dates=dates,
+                )
+
+    if mode == "eval":
+        for epoch in list(range(0, epochs, 10)) + [epochs - 1]:
+            evaluate_all_lags(
+                dates,
+                log_rets,
+                lag_predictions[epoch],
+                symbol,
+                model_type,
+                batch_size,
+                epoch,
+            )
--- src/tabnet.py
+++ src/tabnet.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 from pytorch_tabnet.callbacks import Callback
 from pytorch_tabnet.tab_model import TabNetClassifier
@@ -25,86 +26,98 @@ class ModelCheckpoint(Callback):
             self.model.save_model(os.path.join(final_path, "params"))
 
 
-mode = "train"
-symbols = ["^SPX", "^DAX", "^BET"]
+mode = "eval"
+# symbols = ["^SPX", "^DAX", "^BET"]
+symbols = ["^SPX"]
 symbol = symbols[0]
-model_type = "TNN3"
+model_type = "TNN1"
 epochs = 100
-batch_size = 32
+batch_size = 1024
 lags = 5
 epoch = 20
 
-ds = create_dataset(symbol)
+for symbol in symbols:
+    ds = create_dataset(symbol)
 
-x_train, y_train = ds["train"]["X"], ds["train"]["Y"]
-x_valid, y_valid = ds["valid"]["X"], ds["valid"]["Y"]
-x_test, y_test = ds["test"]["X"], ds["test"]["Y"]
+    x_train, y_train = ds["train"]["X"], ds["train"]["Y"]
+    x_valid, y_valid = ds["valid"]["X"], ds["valid"]["Y"]
+    x_test, y_test = ds["test"]["X"], ds["test"]["Y"]
 
-tabnet_kwargs = {
-    "TNN1": {"n_d": 8, "n_a": 8, "n_steps": 3},
-    "TNN2": {"n_d": 16, "n_a": 16, "n_steps": 3},
-    "TNN3": {"n_d": 32, "n_a": 32, "n_steps": 3},
-}
+    tabnet_kwargs = {
+        "TNN1": {"n_d": 8, "n_a": 8, "n_steps": 3},
+        "TNN2": {"n_d": 16, "n_a": 16, "n_steps": 3},
+        "TNN3": {"n_d": 32, "n_a": 32, "n_steps": 3},
+    }
 
-model = TabNetClassifier(**tabnet_kwargs[model_type])
+    model = TabNetClassifier(**tabnet_kwargs[model_type])
 
-lag_predictions = {}
+    lag_predictions = defaultdict(dict)
 
-for lags in [5, 10, 20]:
-    model_path = os.path.join(
-        "models",
-        model_type,
-        symbol,
-        "lags",
-        str(lags),
-        "batch_size",
-        str(batch_size),
-        "epochs",
-    )
-    os.makedirs(model_path, exist_ok=True)
-
-    if mode == "train":
-        save_every_epochs = ModelCheckpoint(
-            model=model, model_path=model_path, checkpoint_interval=10
+    for lags in [5, 10, 20]:
+        model_path = os.path.join(
+            "models",
+            model_type,
+            symbol,
+            "lags",
+            str(lags),
+            "batch_size",
+            str(batch_size),
+            "epochs",
         )
+        os.makedirs(model_path, exist_ok=True)
 
-        model.fit(
-            x_train[:, :lags],
-            y_train,
-            eval_set=[(x_valid[:, :lags], y_valid)],
-            eval_metric=["logloss"],
-            max_epochs=100,
-            patience=0,
-            batch_size=batch_size,
-            callbacks=[save_every_epochs],
-        )
+        if mode == "train":
+            save_every_epochs = ModelCheckpoint(
+                model=model, model_path=model_path, checkpoint_interval=10
+            )
 
-        plots.loss_curves(
-            model.history["loss"], model.history["val_0_logloss"], model_path
-        )
-    elif mode == "eval":
-        final_path = os.path.join(model_path, str(epoch))
-        model.load_model(os.path.join(final_path, "params.zip"))
+            model.fit(
+                x_train[:, :lags],
+                y_train,
+                eval_set=[(x_valid[:, :lags], y_valid)],
+                eval_metric=["logloss"],
+                max_epochs=100,
+                patience=0,
+                batch_size=batch_size,
+                callbacks=[save_every_epochs],
+            )
 
-        predictions = model.predict(x_test[:, :lags])
+            plots.loss_curves(
+                model.history["loss"],
+                model.history["val_0_logloss"],
+                model_path,
+            )
+        elif mode == "eval":
+            for epoch in list(range(0, epochs, 10)) + [epochs - 1]:
+                final_path = os.path.join(model_path, str(epoch))
+                model.load_model(os.path.join(final_path, "params.zip"))
 
-        lag_predictions[lags] = predictions
+                predictions = model.predict(x_test[:, :lags])
 
-        calculate_stats(predictions, y_test, final_path)
+                lag_predictions[epoch][lags] = predictions
 
-        log_rets = ds["test"]["log_ret"]
-        dates = ds["test"]["dates"]
+                calculate_stats(predictions, y_test, final_path)
 
-        evaluate_predictions(
-            symbol=symbol,
-            model_path=final_path,
-            pred=predictions,
-            true=y_test,
-            log_close=log_rets,
-            dates=dates,
-        )
+                log_rets = ds["test"]["log_ret"]
+                dates = ds["test"]["dates"]
 
-if mode == "eval":
-    evaluate_all_lags(
-        dates, log_rets, lag_predictions, symbol, model_type, batch_size, epoch
-    )
+                evaluate_predictions(
+                    symbol=symbol,
+                    model_path=final_path,
+                    pred=predictions,
+                    true=y_test,
+                    log_close=log_rets,
+                    dates=dates,
+                )
+
+    if mode == "eval":
+        for epoch in list(range(0, epochs, 10)) + [epochs - 1]:
+            evaluate_all_lags(
+                dates,
+                log_rets,
+                lag_predictions[epoch],
+                symbol,
+                model_type,
+                batch_size,
+                epoch,
+            )
